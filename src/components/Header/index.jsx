7d375966f07656afc2e68afc0eb14262dfdca6ec
dev <t.togyzbaev@dev.userstory.ru>
2019-06-26 19:48:01 +0700

    logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './style.scss';
 
 const Header = () => {
-    return <header className="header">Airbnb</header>;
+    return (
+        <header className="header">
+            <img alt="logo" className="header__logo" src={logo} />
+        </header>
+    );
 };
 
 export default Header;
